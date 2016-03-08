7b0535d89ac8bb35e3c481d5ce8c8c86560b7bfa
mrsteele <matt@omnionline.us>
2016-03-08 15:52:47 -0500

    Adding readme, bumping version, and adding comments.

---
+++ README.md
@@ -0,0 +1,73 @@
+# Content Reader
+
+This package is a [contentful.js](https://github.com/contentful/contentful.js) wrapper. Use it to assist with getting entires, assets, and resolve nested object in requests.
+
+### Installation
+
+Include the package locally in your repository.
+
+`npm install @rhm/content-reader --save`
+
+### Basic Usage
+
+The reader registers a client for communicating with the server. First create an instance of the reader with the following:
+
+```
+// The class
+var contentReader = require('@rhm/content-reader');
+
+// This is your space id (provided by contentful)
+var spaceid = 'space-id';
+
+// This is the access token (provided by contentful)
+var accessToken = 'abcdefg1234567';
+
+// If debug is true, contentful will run in preview mode
+var isDebug = false;
+
+// The instance of the reader
+var reader = new contentReader(spaceid, accessToken, isDebug);
+```
+
+### Promises
+
+The wrapper (along with contentful.js) uses a promise-driven architecture. All requests will responde with a promise which can be used to wait for the response to pass or fail.
+
+Assuming our `reader` has been set up from the code-block above, the following can be used to get an entry, where `entry-id` is the id of the entry in contentful.
+
+```
+reader.getEntry('entry-id').then(function (entry) {
+    // assuming there is a title attribute
+    console.log(entry.title);
+});
+```
+
+### Advanced Usage
+
+##### Entry
+
+An entry can be retrived using the following:
+
+```
+reader.getEntry('entry-id', ignoreParse).then(function (entry) {
+    console.log(entry);
+}, function (err) {
+    console.log('something went wrong');
+});
+```
+
+`entry-id` is required, but if `ignoreParsed` is set to `true`, then the response will be returned unparsed and left in the raw state.
+
+##### Assets
+
+An asset can be returned using the following:
+
+```
+reader.getAsset('asset-id', ignoreParse).then(function (asset) {
+    console.log(asset);
+}, function (err) {
+    console.log('something went wrong');
+});
+```
+
+`asset-id` is required, but if `ignoreParsed` is set to `true`, then the response will be returned unparsed and left in the raw state.
--- src/content-reader.js
+++ src/content-reader.js
@@ -3,49 +3,85 @@
 var contentful = require('contentful');
 
 class Controller {
+
+    /**
+     * Creates a new instance of the content-reader, which wraps around contentful.js
+     * @param {String} space - The space to read from.
+     * @param {String} accesstoken - The access token provided by contentful.
+     * @param {Bool=} debug - Whether or not to use the preview mode, or the default host.
+     */
     constructor(space, accesstoken, debug) {
-        
+
         let params = {
             space: space,
             accessToken: accesstoken
         };
-        
+
         if (debug) {
             params.host = 'preview.contentful.com';
         }
-        
+
         this.client = contentful.createClient(params);
     }
 
+    /**
+     * Returns the space for the client.
+     * @returns {Object} Details about the currently registered space.
+     */
     getSpace() {
         return this.client.getSpace();
     }
 
+    /**
+     * Returns a list of entries.
+     * @param {String} name - The entry content type.
+     * @returns {Promise} The promise instance.
+     * @todo - Add additional parameters, instead of listing all.
+     */
     getEntries(name) {
         return this.client.getEntries({
             content_type: name
         });
     }
 
+    /**
+     * Returns an individual entry.
+     * @param {String} id - The entry id.
+     * @param {Bool=} noParse - Ignore parsing.
+     * @returns {Promise} The promise instance.
+     */
     getEntry(id, noParse) {
         console.log("Getting entry: " + id);
         var raw = this.client.getEntry(id);
-        
+
         return noParse ? raw : raw.then(entry => this.parse(entry), err => this.genericError(err));
     }
-    
+
+    /**
+     * Returns an individual asset.
+     * @param {String} id - The asset id.
+     * @param {Bool=} noParse - Ignore parsing.
+     * @returns {Promise} The promise instance.
+     */
     getAsset(id, noParse) {
         console.log("Getting asset: " + id);
         var raw = this.client.getAsset(id);
-        
+
         return noParse ? raw : raw.then(asset => this.parse(asset), err => this.genericError(err));
     }
-    
+
+    /**
+     * Returns a generic object based on the passed in object. This is used to resolve sub-objects in requests.
+     * @param {Object} obj - The subobject in a raw request from contentful.
+     * @param {String} obj.id - The id of the asset. Needed for the forwarded promise.
+     * @param {String} obj.linkType - The type of link. Either 'Asset' or 'Entry'.
+     * @returns {Promise} The promise instance.
+     */
     getGeneric(obj) {
         if (obj && obj.sys) {
             obj = obj.sys;
         }
-        
+
         if (obj && obj.id && obj.linkType) {
             if (obj.linkType === 'Entry') {
                 return this.getEntry(obj.id);
@@ -53,13 +89,21 @@ class Controller {
                 return this.getAsset(obj.id);
             }
         }
-        
+
         // otherwise...
         return new Promise((resolve, reject) => {
-            reject({msg: 'Bad generic object'});
+            reject({
+                msg: 'Bad generic object'
+            });
         });
     }
-    
+
+    /**
+     * Resolved the parsed object recursively with the new object returned.
+     * @param {Object} parsed - The currently parsed object. This object nested somewhere should have a reference to obj.id.
+     * @param {Object} obj - The new object to replace the referenced object in the parsed parameter.
+     * @returns {Bool|undefined} - 
+     */
     _updateParsedRef(parsed, obj) {
         for (var field in parsed) {
             if (field === 'id' && parsed[field] === obj.id) {
@@ -74,14 +118,20 @@ class Controller {
                         return true;
                     } else if (success === undefined) {
                         parsed[field][i] = obj;
+                        return true;
                     }
                 }
             }
         }
-        
+
         return false;
     }
-    
+
+    /**
+     * Creates a promise with a handle on the unparsed object, and the currently parsing object.
+     * @param {Object} obj - The unparsed object. This will wait for the request to resolve before parsing.
+     * @param {Object} parsed - The currently parsed parent object. This should have a reference to obj.id somewhere.
+     */
     _deferredRequest(obj, parsed) {
         return new Promise((resolve, reject) => {
             this.getGeneric(obj).then(sub => {
@@ -91,41 +141,55 @@ class Controller {
         });
     }
 
+    /**
+     * Parses a contentful object and resolves sublinks.
+     * @param {Object} entry - The raw entry object from contentful.
+     * @returns {Promise} The promise instance.
+     * @todo - Currently wouldn't work for collections, but we should make a collection promise that wraps a bunch of these up as long as it can manage.
+     * @todo - The collection parse would have to account for potentially already resolved fields. This asumsed the response has nothing resolved.
+     */
     parse(entry) {
-        
+
         let promise = new Promise((resolve, reject) => {
-            
+
             let parsed = {};
-            
+
             let deferred = [];
-            
+
             parsed.id = entry.sys.id;
             parsed.createdAt = entry.sys.createdAt;
             parsed.updatedAt = entry.sys.updatedAt;
-            
+
             for (var key in entry.fields) {
                 if (Array.isArray(entry.fields[key])) {
                     parsed[key] = [];
                     for (var i in entry.fields[key]) {
-                        parsed[key].push({id: entry.fields[key][i].sys.id});
+                        parsed[key].push({
+                            id: entry.fields[key][i].sys.id
+                        });
                         deferred.push(this._deferredRequest(entry.fields[key][i], parsed));
                     }
                 } else if (entry.fields[key].sys) {
-                    parsed[key] = {id: entry.fields[key].sys.id};
+                    parsed[key] = {
+                        id: entry.fields[key].sys.id
+                    };
                     deferred.push(this._deferredRequest(entry.fields[key], parsed));
                 } else {
                     parsed[key] = entry.fields[key];
                 }
             } // end fields loop
-            
+
             Promise.all(deferred).then(() => {
                 resolve(parsed);
             });
         });
-        
+
         return promise;
     }
-    
+
+    /**
+     * Used to create a generic error should we need one.
+     */
     genericError(err) {
         console.log(err);
         return err;
