d19cfff1fd588b0e8a1db9b044f7cfb54c916031
mrsteele <matt@omnionline.us>
2016-03-11 10:35:41 -0500

    An attempt to allow findEntries similar to findEntry

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var c = require('./index');
 var reader = new c('cyn2ssr0iqx0', '1157ce98f62ebccc473a3fe59397c84fc685e5f1b7630ad56da07ccba3be50ca');
 
-reader.findEntry('slideshow', {slug: 'common-acid-reflux-triggers'}).then(function (entry) {
-    console.log(entry);
-}, function(err) {
+reader.findEntries('slideshow', {slug: 'common-triggers'}).then(entries => {
+    console.log(entries);
+}, err => {
     console.log(err);
 });
