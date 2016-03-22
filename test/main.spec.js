387d97d8c8c78bc8ca84620a958f14af3a9277c7
Tom Jenkins <tjenkins@remedyhealthmedia.com>
2016-03-22 09:14:31 -0400

    fixed test to the format required to test promises in mocha

--- test/main.spec.js
+++ test/main.spec.js
@@ -20,18 +20,15 @@ describe('Content-reader', function () {
     });
 
     it('should return an article when passed the article id', function () {
-      let actual = reader.getEntryById('4V2Ze2uOZy60k8CeUc0Gsu', { include: 1 }).then(entry => {
+      let result = reader.getEntryById('4V2Ze2uOZy60k8CeUc0Gsu', { include: 1 });
+
+      return result.then(function (entry) {
         var parsedEntry = reader.parse.it(entry);
 
         parsedEntry.should.have.deep.property('id', '4V2Ze2uOZy60k8CeUc0Gsu');
         parsedEntry.should.have.property('meta').that.is.an('object');
         parsedEntry.should.have.deep.property('contentType', 'article');
         parsedEntry.should.have.property('fields').that.is.an('object');
-        parsedEntry.should.have.property('bubba').that.is.an('object');
-        console.log('did these pass?');
-      })
-      .catch(err => {
-        console.log(err.stack);
       });
     });
 
