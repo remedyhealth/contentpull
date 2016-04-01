262a0277e67c052554e88907f2f2e8d117052569
mrsteele <matt@omnionline.us>
2016-04-01 16:51:50 -0400

    closes #6 and closes #7
    Added comments and cleaned things.

--- src/parser.js
+++ src/parser.js
@@ -15,18 +15,28 @@ class Parser {
         let parsed = {};
 
         if (obj && obj.sys && (obj.sys.type !== 'Array' || obj.sys.type !== 'Link')) {
+
+            // Add the important stuff
             parsed.id = obj.sys.id;
+            parsed.type = obj.sys.type;
+
+            // Add meta fields
             parsed.meta = {
                 createdAt: obj.sys.createdAt,
                 updatedAt: obj.sys.updatedAt,
                 revision: obj.sys.revision,
             };
+
+            // If it is an entry, probably can add the type
             if (obj.sys.contentType) {
                 parsed.contentType = obj.sys.contentType.sys.id;
             }
 
+            // All fields need to be parsed and added
             parsed.fields = {};
             for (let key in obj.fields) {
+                
+                // It is either going to be a nested object, array, or raw field
                 if (obj.fields[key] && obj.fields[key].sys) {
                     parsed.fields[key] = this.it(obj.fields[key]);
                 } else if (Array.isArray(obj.fields[key])) {
@@ -49,6 +59,8 @@ class Parser {
      * @returns {Object} The parsed object.
      */
     all(obj) {
+
+        // Create the parsed JSON structure
         let parsed = {
             meta: {
                 total: 0
@@ -56,11 +68,14 @@ class Parser {
             items: []
         };
 
+        // If there are many, parse them and populate them
+        // If there is only one, go ahead and add it
         if (obj && obj.sys && obj.sys.type === 'Array') {
             parsed.meta.total = obj.total;
-            parsed.meta.skip = obj.skip;
-            parsed.meta.limit = obj.limit;
             parsed.items = obj.items.map(item => this.it(item));
+        } else if (obj && obj.sys && obj.sys.type) {
+            parsed.meta.total = 1;
+            parsed.items.push(this.it(obj));
         }
 
         return parsed;
@@ -73,8 +88,12 @@ class Parser {
      */
     it(obj) {
         if (obj && obj.sys) {
+            
+            // if it is an array, use
             return (obj.sys.type === 'Array') ? this.all(obj) : this.one(obj);
         } else {
+            
+            // not valid, return an empty object
             return {};
         }
     }
