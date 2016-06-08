38ff0e6405f450b551d31480a40676ed0b7956a7
mrsteele <matt@omnionline.us>
2016-06-08 14:31:58 -0400

    adding things in place for the tests

---
+++ test/stubs/space.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "sys": {
+    "type": "Space",
+    "id": "ww6sikmud9wx"
+  },
+  "name": "Remedy Test and QA",
+  "locales": [
+    {
+      "code": "en-US",
+      "default": true,
+      "name": "U.S. English"
+    }
+  ]
+};
