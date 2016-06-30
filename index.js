fc98c82c4bd57623d8eee10bed6e648190778e9f
mrsteele <matt@omnionline.us>
2016-06-30 14:00:11 -0400

    feat: Adding total es6 support and backwards-compatibility.
    Adding es6, backwards compatibility, and tools to allow older versions of nodejs and iojs to use
    this package.
    
    closes #51

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 let puller;
 try {
-    puller = require('./src/wrapper');
+  puller = require('./dist/wrapper');
 } catch (err) {
+  if (err.code === 'MODULE_NOT_FOUND') {
+    require('babel-register')
+    puller = require('./src/wrapper')
+  } else {
     console.log(err);
     process.exit(1);
 }
 
-module.exports = puller;
+export default puller
